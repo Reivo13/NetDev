adee4247cc903a989474cb9660d615106e3ea286
zacky-nz <zackynur12345@gmail.com>
2025-04-11 11:55:11 +0700

    Update urls.py

--- APOTEK/src/cfehome/urls.py
+++ APOTEK/src/cfehome/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('homeuser/', include('homeuserapp.urls')),
     path('adminapp/',include('adminapp.urls',namespace= "adminapp")),
     path('resepapp/',include('resepapp.urls',namespace= "resepapp")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
