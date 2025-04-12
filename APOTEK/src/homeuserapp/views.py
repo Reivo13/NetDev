8ec14a386ea6b1cf52881f4e3840ca936b60ad1f
ArdeezYy <lucilferardeezy@gmail.com>
2025-04-12 23:19:27 +0700

    Save progress before switching to branch ardika

--- APOTEK/src/homeuserapp/views.py
+++ APOTEK/src/homeuserapp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from daftarobat.models import Obat
+
 
 def homeuser_view(request):
     return render(request, 'homeuserapp/homeuser.html')
+
+def homeuser(request):
+    obat_list = Obat.objects.all()  # Ambil semua obat
+    return render(request, 'homeuser/homeuser.html', {'obat_list': obat_list})
