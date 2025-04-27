7ff85b561c51abb560c6f6362d0acb45f03d19c0
Putri1106 <sandrinaekaputri116@gmail.com>
2025-04-27 23:53:57 +0700

    Admin resep

--- APOTEK/src/cfehome/urls.py
+++ APOTEK/src/cfehome/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, re_path , include
-from adminapp.views import DataObatListView,DataObatDetailView
+from django.urls import path, include
 from django.conf.urls.static import static
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homeuser/', include('homeuserapp.urls')),
-    path('admin/', admin.site.urls),
-    path('adminapp/',include('adminapp.urls',namespace= "adminapp")),
-    path('resepapp/',include('resepapp.urls',namespace= "resepapp")),
+    path('admin_resep/', include('admin_resep.urls')),
+    path('adminapp/', include('adminapp.urls', namespace="adminapp")),
+    path('resepapp/', include('resepapp.urls', namespace="resepapp")),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
