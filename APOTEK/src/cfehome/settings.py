678f17175f74a40ef3bab3a6c19ecde2e557046e
Reivo <akbarreivo44@gmail.com>
2025-05-06 00:53:24 +0000

    landing solve

--- APOTEK/src/cfehome/settings.py
+++ APOTEK/src/cfehome/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'accounts','static'),
     os.path.join(BASE_DIR, 'loginapp', 'template', 'public'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
